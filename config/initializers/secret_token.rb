ecb0c8ee03e5b0b46a97aacc6363b5dd6452ff32
Alex Wochna <alex.wochna@gmail.com>
2014-02-25 12:50:02 -0700

    Initial commit, guard, spork, rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CampaignKeeper::Application.config.secret_key_base = '60cef0e417ad1f6f7059b577f205e11b20e8f5d1f9c67adcea9af0ab021093112b7dd9d2d1998e2b2b26ce771150020e23ca995c0f443ec2408f22e2c149f153'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CampaignKeeper::Application.config.session_store :cookie_store, key: '_campaign_keeper_session'
