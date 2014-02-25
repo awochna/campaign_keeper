b4aba0baf1aae82a6082911059a25b11630b2d39
Alex Wochna <alex.wochna@gmail.com>
2014-02-25 15:31:28 -0700

    Add basic factories for each model

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,5 +9,23 @@ FactoryGirl.define do
   factory :campaign do
     name        "Fall of Mystra"
     backstory   "A mysterious adventures' guild has more power than they should"
+    association :user, factory: :user
+  end
+
+  factory :npc do
+    name  "Kimblee"
+    association :campaign, factory: :campaign
+  end
+
+  factory :encounter do
+    name  "Encounter 1"
+    story "The adventurers find themselves in a cave..."
+    association :campaign, factory: :campaign
+  end
+
+  factory :creature do
+    name  "Flesh Golem"
+    cr    8
+    association :encounter, factory: :encounter
   end
 end
